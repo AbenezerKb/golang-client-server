4bf4d95affb15ee0564d3ec38e872d57f85d3b48
unknown <aben.jacob3@gmail.com>
2022-03-17 11:19:54 +0300

    finalized client server

--- client.go
+++ client.go
@@ -38,25 +38,36 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 
 	jsn, err := ioutil.ReadAll(r.Body)
 
+	//checking if the server is able to read the data and if not send bad request 400
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		log.Fatal("Error reading the body", err)
 	}
 
+	// unmarshaling the JSON data
 	err = json.Unmarshal(jsn, &location)
+
+	//checking if unmarshaling has error
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		log.Fatal("Decoding error: ", err)
 	}
-	log.Printf("Received: %v\n", location)
+
+	//printing the recieved password and xmlstring
+	fmt.Printf("Password: %v\n", location.C2.Password)
+	fmt.Printf("xmlstring: %v\n", location.C2.Xmlstring)
+
 	password := "1234567"
 	hash := md5.Sum([]byte(password))
 
 	//check for the password equality
 	if hex.EncodeToString(hash[:]) == location.C2.Password {
+
 		//create and write the  xml  to file
 		os.Create("C:\\Users\\Administrator\\Desktop\\Web\\code\\third\\success.xml")
 		writer, err := os.OpenFile("C:\\Users\\Administrator\\Desktop\\Web\\code\\third\\success.xml", os.O_RDWR|os.O_APPEND, 0660)
+
+		//if saving to file failed it will send status of 500
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Println("error opening file")
@@ -64,16 +75,20 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		encoder := xml.NewEncoder(writer)
 		er := encoder.Encode(location)
+
+		//if saving to file failed it will send status of 500
 		if er != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Println(er)
 			return
 		}
-
+		fmt.Println("password equality success, success.xml file created and payload data written")
 	} else {
 		//create and write the  xml  to file
 		os.Create("C:\\Users\\Administrator\\Desktop\\Web\\code\\third\\failed.xml")
 		writer, err := os.OpenFile("C:\\Users\\Administrator\\Desktop\\Web\\code\\third\\failed.xml", os.O_RDWR|os.O_APPEND, 0660)
+
+		//if saving to file failed it will send status of 500
 		if err != nil {
 			fmt.Println("error opening file")
 			w.WriteHeader(http.StatusInternalServerError)
@@ -82,11 +97,15 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 
 		encoder := xml.NewEncoder(writer)
 		er := encoder.Encode(location)
+
+		//if saving to file failed it will send status of 500
 		if er != nil {
 			fmt.Println(er)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		fmt.Println("password equality failed, failed.xml file created and payload data written")
 	}
 
 }
@@ -102,17 +121,17 @@ func client() {
 	password := "1234567"
 	xmlstring := "the xml string"
 
-	// the password is encrypted using hash
+	// encrypting the password
 	hash := md5.Sum([]byte(password))
 	hex.EncodeToString(hash[:])
 
-	// initailize the variables xml data
+	// initailizing C2BPayment with the password and xmlstring variables xml data
 	cc := C2BPaymentQueryResult{
 		Xmlstring: xmlstring,
 		Password:  hex.EncodeToString(hash[:]),
 	}
 
-	// inserting the initailized xml's data
+	// initializing the xml with C2BPayment
 	xm := XML1{
 		C2: cc,
 	}
@@ -131,13 +150,16 @@ func client() {
 		log.Fatal(err)
 	}
 
+	//check if the status code of the response is different than 200 and print with error
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Unable to send the data status code: %v", resp.StatusCode)
+		fmt.Println("Unable to send the data, Status code: %v", resp.StatusCode)
 		return
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println("Response: ", string(body))
+	//print the status code
+	fmt.Println("Response: ", resp.Status)
+	//body, err := ioutil.ReadAll(resp.Body)
+	//fmt.Println("Response: ", string(body))
 	resp.Body.Close()
 }
 
@@ -147,5 +169,7 @@ func main() {
 	go server()
 
 	//Run the client in the main
+
 	client()
+
 }
